6aa1a3c27f1b43b4973978ec4802c0081e9fdb12
Shreyas Potnis <shreyaspotnis@gmail.com>
2015-04-15 08:42:35 -0400

    Added a plugin to analyze vertical stern gerlach

--- ipbec/plugins/__init__.py
+++ ipbec/plugins/__init__.py
@@ -1,4 +1,5 @@
 from sterngerlach import PluginDialog as sterngerlach
+from sterngerlachv import PluginDialog as sterngerlachv
 from collage import PluginDialog as collage
 
-plugin_list = [sterngerlach, collage]
+plugin_list = [sterngerlach, sterngerlachv, collage]
---
+++ ipbec/plugins/sterngerlachv/__init__.py
@@ -0,0 +1,1 @@
+from sterngerlachv import PluginDialog
